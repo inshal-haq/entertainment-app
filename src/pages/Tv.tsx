96255ed93d9c848090eb306cc7e162f034b82441
Inshal Haq <inshalulhaq11@gmail.com>
2024-07-15 00:45:27 -0500

    Complete Tv page

--- src/pages/Tv.tsx
+++ src/pages/Tv.tsx
@@ -1,5 +1,43 @@
+import { useState } from 'react'
+import { useAtom } from 'jotai'
+import { totalSearchResultsAtom } from '../util/atoms'
+import { fetchSearchResults, fetchTrending } from '../util/http'
+
+import Search from '../components/Search'
+import MediaList from '../components/MediaList'
+
 const Tv: React.FC = () => {
-	return <div>Tv</div>
+	const [searchTerm, setSearchTerm] = useState('')
+	const [totalSearchResults, setTotalSearchResults] = useAtom(totalSearchResultsAtom)
+
+	return (
+		<>
+			<Search placeholder='Search for TV series' setSearchTerm={setSearchTerm} />
+			{!searchTerm && (
+				<>
+					<MediaList
+						title='TV Series'
+						queryKey={['media', 'tv', 'day']}
+						queryFn={() => fetchTrending({ category: 'tv', time: 'day' })}
+						staleTime={43200000} // 12 hours in ms since api data is a daily list
+						isTopContainer
+					/>
+				</>
+			)}
+			{searchTerm && (
+				<MediaList
+					title={`Found ${totalSearchResults} result${
+						totalSearchResults !== 1 ? 's' : ''
+					} for '${searchTerm}'`}
+					queryKey={['media', 'tv', 'day', { search: searchTerm }]}
+					queryFn={() => fetchSearchResults({ category: 'tv', searchTerm })}
+					setTotalSearchResults={setTotalSearchResults}
+					isShowList
+					isTopContainer
+				/>
+			)}
+		</>
+	)
 }
 
 export default Tv
