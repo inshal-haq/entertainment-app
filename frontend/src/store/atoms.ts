b790e10e7bc0e220b017ee8bdfd4f9bb10ce96a3
Inshal Haq <inshalulhaq11@gmail.com>
2024-08-11 20:53:54 -0500

    Persist bookmarks in localStorage

--- frontend/src/store/atoms.ts
+++ frontend/src/store/atoms.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 import { Media } from '../types/media'
 
-export const bookmarksAtom = atom<Media[]>([])
+export const bookmarksAtom = atomWithStorage<Media[]>('bookmarks', [])
 export const totalSearchResultsAtom = atom<number>(0)
